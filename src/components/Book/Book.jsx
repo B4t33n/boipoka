2c04251970b9cdcbd793652d987ff45beb0a8de9
B4t33n <gamilniyakikorbu@gmail.com>
2025-04-17 01:37:31 +0600

    added books Publishing year

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,28 +1,36 @@
 import React from "react";
+import { Star } from "lucide-react";
 
 const Book = ({ book }) => {
-  const { bookName, author, image, tags } = book;
+  const { bookName, author, image, tags, yearOfPublishing
+  } = book;
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
       <figure>
-        <img className="w-2/5" src={image} alt="Shoes" />
+        <img className="w-1/2 md:w-2/5" src={image} alt="Books" />
       </figure>
       <div className="mt-10 space-x-10">
-        {tags.map((tag,index) => (
-          <div key={index} className="badge bg-green-50 p-4 text-green-600 rounded-full">{tag}</div>
+        {tags.map((tag, index) => (
+          <div
+            key={index}
+            className="badge bg-green-50 p-4 text-green-600 rounded-full"
+          >
+            {tag}
+          </div>
         ))}
       </div>
       <div className="card-body">
         <h2 className="card-title">
           {bookName}
-          <div className="badge badge-secondary">NEW</div>
+          <div className="badge badge-secondary">{yearOfPublishing
+          }</div>
         </h2>
+        <div></div>
         <div>
+          <p className="text-start text-gray-500 font-bold text-md">
+            By: {author}
+          </p>
         </div>
-        <div className="card-actions justify-around">
-
-        </div>
-        <p className="text-start text-gray-500 font-bold text-md">By: {author}</p>
       </div>
     </div>
   );
