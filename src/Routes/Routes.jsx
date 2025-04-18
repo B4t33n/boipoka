dfb8fa0920e0375d080240a794c410c16b85c062
B4t33n <gamilniyakikorbu@gmail.com>
2025-04-18 22:46:21 +0600

    added error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import { createBrowserRouter } from "react-router";
 import Root from "../components/Root/Root";
 import Home from "../components/Home/Home";
 import About from "../components/About/About";
+import Error from "../components/Error/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
+    errorElement: <Error></Error>,
     children: [
       {
         index: true,
---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className='text-center text-5xl mt-68 font-bold'>
+            <h1>Fuck man....!</h1>
+            <p>What are you doing here..!</p>
+            <p className='text-red-500'>404</p>
+        </div>
+    );
+};
+
+export default Error;
